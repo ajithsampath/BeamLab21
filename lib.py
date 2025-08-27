23d28361652d738765ec583541b0e0e0dab86b95
Ajith <ajithsampath1997@gmail.com>
2025-08-27 09:19:29 +0200

    First push

---
+++ lib.py
@@ -0,0 +1,75 @@
+#Author: Ajith Sampath
+#Affiliation: University of Geneva
+#Project: HIRAX Beam package
+
+#lib.py file for all the helper functions
+
+import numpy as np
+from scipy.special import jn
+
+def NollToQuantum(j):
+    n=int(np.ceil((-3+np.sqrt(9+(8*j)))/2))
+
+    m=int((2*j)-(n*(n+2)))
+
+    return (n, m);
+
+
+def QuantumToNoll(n,m):
+    
+    j=int((n*(n+2)+m)/2)
+    return j;
+    
+
+def basis_cart(sigx,sigy,x,y,N):
+    '''Routine to generate Zernike Transforms (basis) from Bessel function of first kind'''
+    x,y = np.meshgrid(x/sigx,y/sigy)
+    r = np.hypot(x,y)
+    r[r==0] = 1e-10
+    #rho = rho/sig
+    theta = np.arctan2(y,x)
+
+    z1 = np.zeros((N,len(r.flatten())),dtype=np.complex128)
+
+    count = 0
+
+    for j in range(0,N):
+        n,m = NollToQuantum(j)
+        if n>=0 and n>=abs(m) and (n-abs(m))%2==0:
+            Bes=(jn(n+1,r))/r
+            nc = (((2*n+1)*(2*n+3)*(2*n+5))/(-1)**n)**0.5
+            temp=((nc*(np.exp(1j*m*theta))/((1j**m)*2*np.pi) *(-1)**((n-m)/2) *Bes))
+            z1[count]=temp.flatten()
+            count+=1
+        
+    return z1;
+
+
+def basis_pol(sigx,sigy,r_max,nr,nt,N):
+    '''Routine to generate Zernike Transforms (basis) from Bessel function of first kind'''
+    
+    r = np.linspace(0, r_max, nr)
+    theta = np.linspace(0, 2*np.pi, nt)
+
+    r[r==0] = 1e-10
+
+    R, Theta = np.meshgrid(r, theta, indexing='ij')
+
+    R_ell = R * np.sqrt((np.cos(Theta)/sigx)**2 + (np.sin(Theta)/sigy)**2)
+
+    z1 = np.zeros((N,len(R_ell.flatten())),dtype=np.complex128)
+
+    count = 0
+
+    for j in range(0,N):
+        n,m = NollToQuantum(j)
+        if n>=0 and n>=abs(m) and (n-abs(m))%2==0:
+            Bes=(jn(n+1,R_ell))/R_ell
+            nc = (((2*n+1)*(2*n+3)*(2*n+5))/(-1)**n)**0.5
+            temp=((nc*(np.exp(1j*m*Theta))/((1j**m)*2*np.pi) *(-1)**((n-m)/2) *Bes))
+            z1[count]=temp.flatten()
+            count+=1
+        
+    return (R_ell,Theta,z1);
+
+
